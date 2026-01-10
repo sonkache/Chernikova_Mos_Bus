e97008ca80681cbbd2f0de039688e182f897a5b0
Sofia <sonkache@mail.ru>
2026-01-10 23:08:19 +0300

    Добавлено отображение остановок на карте

--- api/get_stops.php
+++ api/get_stops.php
@@ -5,7 +5,6 @@ header('Content-Type: application/json; charset=utf-8');
 $rows = dbQuery("
   SELECT id, name, latitude, longitude
   FROM stops
-  WHERE latitude IS NOT NULL
-    AND longitude IS NOT NULL
+  WHERE latitude IS NOT NULL AND longitude IS NOT NULL
 ")->fetchAll();
 echo json_encode($rows, JSON_UNESCAPED_UNICODE);
